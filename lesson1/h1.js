4b3ddda815a6fca8514571af709c7bcdf42d5975
Malev1ch <130825280+Malev1ch@users.noreply.github.com>
2023-08-17 22:29:00 +0600

    Add files via upload

---
+++ lesson1/h1.js
@@ -0,0 +1,46 @@
+//Hw1 - Вывод приветствия после ввода данных
+
+// const userName = prompt ("Enter your name")
+// const userSecondName = prompt ("Enter your second name")
+// let userWelcome = userName + " " + userSecondName
+// alert("Hello dear:" + userWelcome)
+// console.log('Hello dear:' + `${userWelcome}`);
+
+//Hw1 - Вывод приветствия после ввода данных
+
+//HW2 - Сравнение чисел >=<
+
+// var a = prompt("Введите число", "");
+// var b = prompt("Введите число", "");
+
+// if (isNaN(a) || isNaN(b)) {
+//     alert("Ошибка! Введите числа.");
+// } else if (a > b) {
+//     alert(a + " больше " + b);
+// } else if (a < b) {
+//     alert(b + " больше " + a);
+// } else {
+//     alert("Числа равны");
+// }
+
+//isNaN - проверяет соответствие на числа, если будет буква или символ, выдаст ошибку в алерт.
+
+//HW2 - Сравнение чисел >=<
+
+
+// HW3 - Светофор
+
+// const color = prompt("Задайте из следующий цветов  красный , жёлтый и зелёный")
+
+// if (color === "красный") {
+//     alert("стоять")
+// } else if (color === "жёлтый") {
+//     alert("ждать")
+// } else if (color === "зелёный") {
+//     alert("идти")
+// } else {
+//     alert("Введите цвет из списка (красный, желтый, зеленый)")
+// }
+
+// Тут есть проверка на корректные цвета. Если юзер вводит цвета не соответствуют 3 требуемым цветам, то выдаст ошибку.
+// HW3 - Светофор
