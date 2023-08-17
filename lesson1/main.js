4e3760c5328274dc35d0d64bc62c947b97e8625f
Malev1ch <shadowqq@rambler.ru>
2023-08-17 17:23:41 +0600

    test save

---
+++ lesson1/main.js
@@ -0,0 +1,27 @@
+console.log('Hello');
+
+
+//ES5//
+// var num = 5
+// console.log(num);
+
+// var firstName, lastName;
+// firstName = "Alex";
+// lastName = "Malevich";
+// console.log(firstName, lastName);
+
+//ES5///
+
+//ES6//
+
+
+//let - изменяемая
+// let num = 5
+// num = 2
+// console.log(num);
+//let - изменяемая
+
+//const - неизменяемая
+// const b = 5
+// console.log(b); 
+//const - неизменяемая 
